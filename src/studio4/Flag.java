015cd707614a1487c44f17b1f0dec96e80dd4726
thomastelesco <thomastelesco@localhost>
2024-10-03 11:05:01 -0500

    Studio 4 Finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,30 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
-		
+	
+	public static void main(String[] args) 
+	{
+		StdDraw.setPenColor(5, 148, 0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.4);
+		StdDraw.setPenColor(209, 14, 18);
+		//StdDraw.filledCircle(0.5, 0.7, 0.05);
+		StdDraw.filledRectangle(0.5, 0.7, 0.03, 0.13);
+		StdDraw.setPenColor(150, 110, 15);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledRectangle(0.5, 0.75, 0.03, 0.009);
+		StdDraw.filledCircle(0.35, 0.65, 0.05);
+		StdDraw.filledCircle(0.65, 0.65, 0.05);
+		StdDraw.filledEllipse(0.5, 0.45, 0.07, 0.04);
+		StdDraw.filledCircle(0.55, 0.55, 0.02);
+		StdDraw.filledCircle(0.45, 0.55, 0.02);
+		StdDraw.filledCircle(0.45, 0.55, 0.02);
+		StdDraw.filledEllipse(0.5, 0.36, 0.09, 0.025);
+		StdDraw.filledRectangle(0.5, 0.385, 0.015, 0.03);
+		StdDraw.filledRectangle(0.5, 0.385, 0.015, 0.03);
+		StdDraw.setPenColor(209, 14, 18);
+		StdDraw.filledRectangle(0.5, 0.7, 0.08, 0.01);
+
 	}
+	
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,31 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		
+		if(shape.equals("rectangle")) {
+			int red = in.nextInt();
+			int blue = in.nextInt();
+			int green = in.nextInt();
+			
+			Color color = new Color(red, blue, green);
+			StdDraw.setPenColor(color);
+			
+			boolean filled = in.nextBoolean();
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			
+			if(filled)
+			{
+				StdDraw.filledRectangle(x, y, halfHeight, halfWidth);
+				
+			}
+			else 
+			{
+				StdDraw.rectangle(x, y, halfHeight, halfWidth);
+			}
+		}
 	}
 }
